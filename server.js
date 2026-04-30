4fd648b64234eb6bf301e1cd5125bfb5b1b56b08
Zachary Michael Hugo Reyes <111546468+zzacharymichael@users.noreply.github.com>
2026-04-30 13:30:22 +0800

    Update Flutter Web ports in server.js

--- server.js
+++ server.js
@@ -26,8 +26,8 @@ const allowedOrigins = [
   'http://192.168.1.10:3000',
   'http://localhost',
   'http://192.168.1.10',
-  'http://localhost:63760',      // Flutter Web on Chrome
-  'http://127.0.0.1:63760',      // Flutter Web on Chrome (alternate)
+  'http://localhost:54326',      // Flutter Web on Chrome
+  'http://127.0.0.1:54326',      // Flutter Web on Chrome (alternate)
   'capacitor://localhost',
   'ionic://localhost',
   'http://localhost:8080',
